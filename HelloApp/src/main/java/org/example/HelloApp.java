a1f5a96ff0e30fdfe31e8152631f79303229abd6
Tosha <toshanidutta@gmail.com>
2026-03-30 14:42:19 +0530

    added UC2

--- HelloApp/src/main/java/org/example/HelloApp.java
+++ HelloApp/src/main/java/org/example/HelloApp.java
@@ -3,7 +3,14 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class HelloApp {
-   public static void main(String args[]){
-       System.out.println("Hello World");
-   }
+
+    public static void main(String[] args) {
+        String name;
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+        System.out.println("Hello, " + name + "!");
+    }
 }
