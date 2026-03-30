d2926a506f69813861016a232000669ea213100b
Tosha <toshanidutta@gmail.com>
2026-03-30 14:59:10 +0530

    added UC4

--- HelloApp/src/main/java/org/example/HelloApp.java
+++ HelloApp/src/main/java/org/example/HelloApp.java
@@ -6,12 +6,26 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        String name="World";
+        String names;
 
         if (args.length > 0) {
-            name = args[0];
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                builder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    builder.append(", ");
+                }
+            }
+
+            names = builder.toString();
+
+        } else {
+            names = "World";
         }
 
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + names + "!");
     }
 }
