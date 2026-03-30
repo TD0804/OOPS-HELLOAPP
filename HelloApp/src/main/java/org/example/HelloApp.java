5c05e2224d3a0e5b12c71a8cac2cd0e0512434da
Tosha <toshanidutta@gmail.com>
2026-03-30 15:21:51 +0530

    added UC7

--- HelloApp/src/main/java/org/example/HelloApp.java
+++ HelloApp/src/main/java/org/example/HelloApp.java
@@ -6,17 +6,14 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
+        String names;
+
         if (args.length == 0) {
-            System.out.println("Hello, World!");
+            names = "World";
         } else {
-
-            StringBuilder nameBuilder = new StringBuilder();
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-            System.out.println("Hello, " + names + "!");
+            names = String.join(", ", args);
         }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
